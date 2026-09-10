b2d39f6692f4cd59b3d9076d88436cffbadcbb65
Nimit Goyal <goyal.nimit@gmail.com>
2026-09-10 15:38:52 +0530

    committing project structure

--- app/db/models/channel.py
+++ app/db/models/channel.py
@@ -4,7 +4,6 @@ from sqlalchemy import DateTime, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.base import Base
-
 class Channel(Base):
     __tablename__ = "channels"
 
